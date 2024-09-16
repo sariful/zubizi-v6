8487ca6bd08f188f8cfbe1f4c0fa74c5cdde5c4f
Sariful <sarifulislam.molla@gmail.com>
2024-09-16 20:53:08 +0530

    added block

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './source/html/**/*.html',  // Adjust this based on your file structure
     './source/js/**/*.js',      // For any JavaScript files that use Tailwind classes
   ],
   theme: {
+    extend: {
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(to right, #FF7E5F, #FEB47B)',
+      },
+      colors: {
+        cyan: colors.cyan,
+      },
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
